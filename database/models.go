34db6a498ad52d4052c7ecc0b8c2ee3a125881cf
stefan safranek <stefan@stefan.stefan>
2019-08-06 14:14:40 -0700

    export of measurement data

---
+++ database/models.go
@@ -0,0 +1,21 @@
+package database
+
+type LocationMeasurements struct {
+	LocationId         string                `json:"location_id"`
+	SensorMeasurements []*SensorMeasurements `json:"sensors"`
+}
+
+type SensorMeasurements struct {
+	SensorId           string                `json:"sensor_id"`
+	BucketMeasurements []*BucketMeasurements `json:"buckets"`
+}
+
+type BucketMeasurements struct {
+	BucketId     int64          `json:"bucket_id"`
+	Measurements []*Measurement `json:'measurements'`
+}
+
+type Measurement struct {
+	Key   string  `json:"key"`
+	Value float64 `json:"value"`
+}
